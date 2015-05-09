903eafffa04d9e1a8f662a63c939aff5c7e1227a
David Mahon <david.m.mahon@gmail.com>
2015-05-09 18:35:15 -0500

    user signin

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -1,7 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe WelcomeController, type: :controller do
-  it "renders the index template" do
+  it "redirects to the sign in page when logged out" do
+    sign_out :user
+    get :index
+    expect(response).to redirect_to(new_user_session_path)
+  end
+
+  it "displays the index page when logged in" do
+    user = build(:user)
+    user_params = {name: user.name, password: user.password, email: user.email}
+    user = User.create(user_params)
+    sign_in user
     get :index
     expect(response).to render_template('index')
   end
